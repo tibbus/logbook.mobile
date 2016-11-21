3bbea2f71a356fa4d7d52c54fc7969f35f41f24d
Amila17 <amila.prabandhika@bedegaming.com>
2016-11-21 20:35:55 +0000

    Added init.sh file

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm install react-native
+npm install react
+npm install -g react-native-cli
+npm install react-native-video
+npm install react-native-vector-icons
+npm install react-native-image-picker
