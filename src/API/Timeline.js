553a6d173f397f0f1c5f71eaa10cc67ef3f5de4b
Amila17 <amila.prabandhika@bedegaming.com>
2016-11-30 22:52:06 +0000

    Updated the promise code.

--- src/API/Timeline.js
+++ src/API/Timeline.js
@@ -1,4 +1,37 @@
 import { fetcher } from './fetch'
 import { api } from './config'
 
-export const getTimeline = fetcher(api + 'timeline/{carInfoId}')
+
+export const getTimelineToken = fetcher(api + 'feeds/token', 'POST',) 
+
+const getOldTimeline = fetcher(api + 'timeline/{carInfoId}')  
+
+
+export const getTimeline = () => { 
+
+    var promise  = new Promise((resolve, reject) => { 
+
+      const body = {actorId:1, actorType:"user" } 
+      const request = { body } 
+
+      getTimelineToken(request, {}) 
+      .then(data => { 
+
+        console.log(data) 
+        console.log(client) 
+        resolve(data)   
+
+      }) 
+
+      .catch(e => { 
+        console.log(e) 
+
+        // TODO Handle timeline init failure 
+
+      }) 
+    }) 
+
+    //fetcher to get token 
+    //One token available, use fetch to call getstream? 
+    return promise; 
+} 
