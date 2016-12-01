31d9aff20ea3af12c944a6a6e6990ee99171cb5a
Tiberiu Popescu <tiberiu.popescu@williamhill.com>
2016-12-01 22:46:07 +0100

    fix TimeLine geStream promises

--- src/API/Timeline.js
+++ src/API/Timeline.js
@@ -2,45 +2,38 @@ import { fetcher } from './fetch'
 import { api } from './config'
 import getStream from 'getstream';
 
-export const getTimelineToken = fetcher(api + 'feeds/token', 'POST',) 
+export  const getTimelineToken  =  fetcher(api  +  'feeds/token',  'POST')
 
-const getOldTimeline = fetcher(api + 'timeline/{carInfoId}')  
-
+const  getOldTimeline  =  fetcher(api  +  'timeline/{carInfoId}')
 const getStreamClient = getStream.connect("8r2y2gbevg9j")
 
-export const getTimeline = (carInfoId) => { 
+export  const getTimeline  =  (carInfoId)  =>  {
+    console.log('get timeline')
+        var  promise   =  new  Promise((resolve,  reject)  =>  {
+              const body  =  { actorId: carInfoId,  actorType: 'car'  }
+              const request  =  {  body  }
 
-    var promise  = new Promise(carInfoId, (resolve, reject) => { 
+                // Fetch the token
+              getTimelineToken({ body },  {})
+                .then(token =>  {
+                //Call to timeline
+                const carFeed = getStreamClient.feed('car', carInfoId.toString(), token);
 
-      const body = {actorId:carinfoId, actorType:"car" } 
-      const request = { body } 
+                carFeed.get({ limit: 10 })
+                    .then(body => {
+                        console.log(body);
+                        resolve(body);
+                    })
+                    .catch(response => {
+                        console.log(response);
+                    })
+                        //Get the data and then resolve the data.
+                        console.log(token)
+                  })
+                  .catch(e  =>  {
+                        console.log(e)
+                  })
+        })
 
-     // Fetch the token
-      getTimelineToken(request, {}) 
-      .then(token=> { 
-
-        //Call to timeline
-        const carFeed = getStreamClient.feed('car', carInfoId.toString(), data);
-
-        carFeed.get({limit:10})
-        .then(body => {
-
-            console.log(body);
-            resolve(body);
-
-        })
-        .catch(response => {
-
-        })
-        //Get the data and then resolve the data.
-        console.log(token) 
-      }) 
-
-      .catch(e => { 
-        console.log(e) 
-      }) 
-    
-
-    }) 
-    return promise; 
+        return  promise;
 } 
