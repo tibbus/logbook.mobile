e1110250e3c4387ea2f91f45048516daa2cb3408
Amila17 <amila.prabandhika@bedegaming.com>
2016-12-04 18:53:07 +0000

    Task/TimelinefromGetStream
    Fixed formatting issues.

--- src/API/Timeline.js
+++ src/API/Timeline.js
@@ -9,30 +9,29 @@ const  getOldTimeline  =  fetcher(api  +  'timeline/{carInfoId}')
 
 export  const getTimeline  =  (carInfoId)  =>  {
     console.log('get timeline')
-        var  promise   =  new  Promise((resolve,  reject)  =>  {
-              const body  =  { actorId: carInfoId,  actorType: 'car'  }
-              const request  =  {  body  }
+    var  promise   =  new  Promise((resolve,  reject)  =>  {
+      const body  =  { actorId: carInfoId,  actorType: 'car'  }
+      const request  =  {  body  }
 
-                // Fetch the token
-              getTimelineToken({ body },  {})
-                .then(token =>  {
-                    console.log(token)
-                
-                    //Call to timeline
-                    getFeedData('car', carInfoId, token.token)
-                    .then(response => {
-                        console.log(response);
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    })
-                        
-                  })
-                  .catch(e  =>  {
-                        console.log(e)
-                  })
+        // Fetch the token
+      getTimelineToken({ body },  {})
+        .then(token =>  {
+            console.log(token)
+        
+            //Call to timeline
+            getFeedData('car', carInfoId, token.token)
+            .then(response => {
+                console.log(response);
+                resolve(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+          })
+          .catch(e  =>  {
+                console.log(e)
+          })
         })
 
-        return  promise;
-} 
+    return  promise;
+}
