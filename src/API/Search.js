38b8ae3432fcab2acef3849d43e3f4b4a5a96e76
Amila17 <amila.prabandhika@bedegaming.com>
2017-01-02 17:01:50 +0000

    Task/SearchApi
    Updated the config class with the configuration required for the search api.
    Added a new Search.js file into the API folder to perform the api calls to azure search.
    Removed the unused fetcher import from GetStream.js

---
+++ src/API/Search.js
@@ -0,0 +1,43 @@
+import { fetcher } from './fetch'
+import { searchEnvironment } from './config'
+import { AZURE_SEARCH_API_VERSION } from './config'
+import { searchIndex} from './config'
+import { searchApiKey } from './config'
+import { replaceParams } from '../Utils'
+
+const apiKey = searchApiKey();
+const searchIndex = searchIndex();
+
+const getUrl = (uriParams = { 'api-version': AZURE_SEARCH_API_VERSION }) => {
+    
+    const env = searchEnvironment();
+    const uri = searchIndex + `docs`;
+    
+    if (uriParams) {
+        return env + replaceParams(uri, uriParams, encodeURIComponent);
+  }
+  
+  return env + uri;
+}
+
+export const getSearchResult = (searchTerm) => {
+
+    const url = getUrl();
+    const searchBody = { 'search': searchTerm+'*' };
+
+    return global.fetch(url, {
+        methods: 'post',
+        headers: {
+            'api-key' : apiKey,
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body: searchBody    
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
--- src/API/config.js
+++ src/API/config.js
@@ -1,11 +1,15 @@
 const CURRENT_ENV = 'dev'
 const API_VERSION = 1
 const STREAM_API_VERSION = '1.0'
+const AZURE_SEARCH_API_VERSION = '2015-02-28'
 
 const environments = {
   dev: {
     uri: 'mycarbioservice.azurewebsites.net',
     authUri: 'mycarbioidentity.azurewebsites.net',
+    searchUri: 'https://mycarbiosearch.search.windows.net/indexes/',
+    searchIndex: 'carinfostaging',
+    searchApiKey: '56204AA4847BE7E6D4F8ACFB6502D7C6',
     getStreamUri: 'https://api.getstream.io',
     getStreamApiKey: '8r2y2gbevg9j',
     getStreamFetchLimit: 10,
@@ -23,6 +27,21 @@ export const getIdentityEnvironment = env => {
   const { secure, authUri } = environments[env || CURRENT_ENV]
   const protocol = secure ? 'https' : 'http'
   return `${protocol}://${authUri}/`;
+}
+
+export const getSearchEnvironment = env => {
+  const { searchUri } = environments[env || CURRENT_ENV]
+  return `${searchUri}/`;
+}
+
+export const getSearchIndex = env => {
+  const { searchIndex } = environments[env || CURRENT_ENV]
+  return `${searchIndex}/`;
+}
+
+export const getSearchApiKey = env => {
+  const { searchApiKey } = environments[env || CURRENT_ENV]
+  return `${searchApiKey}`;
 }
 
 export const getStreamEnvironment = env => {
