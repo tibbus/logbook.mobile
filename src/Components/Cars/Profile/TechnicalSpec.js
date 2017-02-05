6dd13235aad374d800af6d0b09a5ad075b8bcbc3
Amila17 <amila.prabandhika@bedegaming.com>
2017-02-05 18:19:55 +0000

    Task/CarProfileTechnicalSpec
    Added a view to show the technical spec for the car.

---
+++ src/Components/Cars/Profile/TechnicalSpec.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    View,
+    Text,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+
+export class Gallery extends Component {
+
+    constructor (props) {
+        super(props)
+    }
+
+    render () {
+
+        return (
+            <View>
+                <View>
+
+                </View>
+            </View>
+        )
+    }
+}
