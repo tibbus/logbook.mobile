69a1e4bb583f021bb18a88e74faf21fcead036a7
Amila17 <amila.prabandhika@bedegaming.com>
2017-02-20 23:30:10 +0000

    Task/AddPostFunctionality
    Added media picker functionality.

--- src/Components/Scenes/AddPost.js
+++ src/Components/Scenes/AddPost.js
@@ -9,6 +9,119 @@ import {
 } from 'react-native'
 import ModalDropdown from 'react-native-modal-dropdown'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import MediaPicker from 'react-native-image-picker'
+
+const getParams = (origURL, uri) => {
+  if (origURL) {
+    return paramsToObj(origURL)
+  }
+
+  const ext = last(uri.split('.'))
+  return {
+    ext,
+    id: 'anonymous',
+    uri
+  }
+}
+
+
+const onGalleryPress = (mediaType) => {
+  const opts = { mediaType };
+  const title = mediaType === 'video' ? 'Videos' : 'Photos'
+  const config = { ...opts, title, chooseFromLibraryButtonTitle: 'Choose from Library...' }
+
+  
+  MediaPicker.launchImageLibrary(config, (response) => {
+
+    console.log('Response : ', response);
+    
+    const { didCancel } = response
+    
+    if(didCancel) {
+      console.log(response);
+    }
+
+    if(error) {
+      console.log(response);
+    }
+
+    console.log(response);
+
+  })
+}
+
+const onCameraPress = () => {
+  MediaPicker.launchCamera(config, response => {
+    const {didCancel, error, data, uri, origUri, fileName, } = response;
+
+    if(didCancel) {
+      console.log(response);
+    }
+
+    if(error) {
+      console.log(response);
+    }
+
+    console.log(response);
+  })
+}
+
+const onCancelPress = (navigator) => {
+  navigator.pop()
+}
+
+export class AddPost extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={styles.headerButtonContainer}>
+            <TouchableHighlight style={styles.headerButton}>
+                <Text>Cancel</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.headerHeadingContainer}>
+            <Text style={styles.headerHeadingTextt}>ADD POST</Text>
+          </View>
+        </View>
+        <View style={styles.carSelectorContainer}>
+            <ModalDropdown options={['image car 1', 'image car 2']} />
+        </View>
+        <View style={styles.contentDescriptionContainer}>
+          <Image source={{uri: 'https://maxcdn.icons8.com/iOS7/PNG/75/Users/user_male_circle_filled-75.png'}} style={styles.icon} />
+          <TextInput placeholder="What's new with car term today? Feeling like: Cruising? Thing? Or something?" returnKeyType='done' multiline={true} style={{flex:1, marginTop: 20, paddingVertical: 10, borderWidth: 1, borderColor: '#000000'}}/>
+        </View>
+        <View style={{flex:1, borderBottomWidth: 2, borderBottomColor: '#000000'}}>
+        </View>
+        <View style={styles.tagsContainer}>
+            <Text>Tags</Text>
+            <Text>Car Make</Text>
+            <Text>Car Model</Text>
+            <Text>Car Year</Text>
+        </View>
+        <View style={styles.contentContainer}>
+            <TouchableHighlight onPress={() => onGalleryPress('image')}>
+              <Text>Image</Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => onGalleryPress('video')}>
+              <Text>Video</Text>
+            </TouchableHighlight>
+            <TouchableHighlight>
+              <Text>Camera</Text>
+            </TouchableHighlight>
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -43,7 +156,6 @@ const styles = StyleSheet.create({
   contentDescriptionContainer: {
     flex: 1,
     flexDirection: 'column',
-    paddingHorizontal: 10
   },
   tagsContainer: {
     flex: 1,
@@ -55,56 +167,10 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
 
-  }
+  },
+  icon: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
 })
-
-export class AddPost extends Component {
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      text: ''
-    }
-  }
-
-  render () {
-    const { carRegSubmit } = this.props
-    return (
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <View style={styles.headerButtonContainer}>
-            <TouchableHighlight style={styles.headerButton}>
-                <Text>Cancel</Text>
-            </TouchableHighlight>
-          </View>
-          <View style={styles.headerHeadingContainer}>
-            <Text style={styles.headerHeadingTextt}>ADD POST</Text>
-          </View>
-        </View>
-        <View style={styles.carSelectorContainer}>
-            <ModalDropdown options={['image car 1', 'image car 2']} />
-        </View>
-        <View style={styles.contentDescriptionContainer}>
-            <Image source={{uri: 'https://maxcdn.icons8.com/iOS7/PNG/75/Users/user_male_circle_filled-75.png'}} style={styles.icon} />
-            <TextInput placeholder="What's new with car term today? Feeling like: Cruising? Thing? Or something?" returnKeyType='done' />
-        </View>
-        <View>
-        </View>
-        <View style={styles.tagsContainer}>
-            <Text>Tags</Text>
-            <Text>Car Make</Text>
-            <Text>Car Model</Text>
-            <Text>Car Year</Text>
-        </View>
-        <View style={styles.contentContainer}>
-            <TouchableHighlight>
-              <Text>Gallery</Text>
-            </TouchableHighlight>
-            <TouchableHighlight>
-              <Text>Camera</Text>
-            </TouchableHighlight>
-        </View>
-      </View>
-    )
-  }
-}
