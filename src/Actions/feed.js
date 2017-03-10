67ad69ae0df79b556f2ea8a2d88b44a3e6d7e636
P G Amila Prabandhika <amila.prabandhika@bedegaming.com>
2017-03-10 00:56:38 +0000

    Task/MyFeed
    Added a feed.js action with the code to call the getTimelineApi
    Updated the timeline api to be more resuable by passing in a type required.
    Added a Feed scene component.
    Updated the scene index.js

---
+++ src/Actions/feed.js
@@ -0,0 +1,15 @@
+import { getTimeline } from '../API/Timeline'
+
+export const getUserFeed = ({
+  userId
+}) => {
+  return dispatch => {
+    getTimeline('user', userId , { userId })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(e => {
+        // TODO Handle timeline init failure
+      })
+  }
+}
---
+++ src/Components/Scenes/Feed.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+import { connect } from 'react-redux';
+import { getUserFeed } from '../../Actions/feed'
+
+export class Feed extends Component {
+    
+    constructor(props) {
+        super(props)
+
+        const { user, dispatch } = this.props;
+
+        dispatch(getUserFeed(user.id))
+    }
+
+    render () {
+        return (
+            <View>
+                <Text>User feed</Text>
+            </View>
+        )
+    }
+
+}
