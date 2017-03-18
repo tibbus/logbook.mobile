a67f5b74b364a0514243049c374285016bb2998b
P G Amila Prabandhika <amila.prabandhika@bedegaming.com>
2017-03-18 14:55:54 +0000

    Task/FixProfileScroll
    Fixed profile scroll.

--- src/Components/Scenes/Profile.js
+++ src/Components/Scenes/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { View } from 'react-native'
+import { ScrollView, StyleSheet } from 'react-native'
 import { Main } from '../Profile'
 import { CarGarage } from '../Cars'
 import { updateUserFollowCount, updateUserCars } from '../../Actions/user.js'
@@ -22,10 +22,32 @@ export class Profile extends Component {
     render() {
         const { user, cars, navigator } = this.props;
         return (
-            <View style={{ flex:1 }}>
-                <Main user = {user} />
-                <CarGarage cars = {cars} user = {user} navigator = {navigator} />
-            </View>
+            <ScrollView
+                automaticallyAdjustContentInsets={false}
+                style={[styles.scrollView, styles.horizontalScrollView]}>
+                    {<Main user = {user} />}
+                    {<CarGarage cars = {cars} user = {user} navigator = {navigator} />}
+            </ScrollView>
         )        
     }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 40,
+    //allowFontScaling: true
+  },
+  scrollView: {
+    flex: 1,
+  },
+  horizontalScrollView: {
+    height: 200,
+  },
+  textHeading: {
+    fontSize: 20,
+    fontWeight: '700',
+    paddingBottom: 10
+  }
+})
