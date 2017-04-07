970c173e63395b7f72f71a6a751a529346964a10
Amila17 <amila.prabandhika@bedegaming.com>
2017-04-07 17:40:46 +0100

    Task/GetRealFollowersData
    Added the logic required to get the real followers data.
    Updated the cars action to get followers from get stream.
    Added a new type to set the followers count.
    Updated the user cars api call.
    Updated the CarProfileInfo view to get the details from a carStats object.
    Updated the CarProfileScene to call the getCarFollowersCount data.
    Updated the profile call to pass in the user id.
    Updated the model in the cars reducer.

--- src/Actions/cars.tsx
+++ src/Actions/cars.tsx
@@ -4,7 +4,8 @@ import {
     UNSET_LOADING_STATUS,
     UPDATE_BROWSING_CAR_CONTENT,
     SET_BROWSING_CAR,
-    FOLLOW_CAR
+    FOLLOW_CAR,
+    SET_FOLLOWER_COUNT
 } from './Types'
 import {
     getCarByRegistration,
@@ -15,6 +16,8 @@ import {
     followCar as followCarApi,
     unFollowCar as unFollowCarApi
 } from '../API/Car'
+import { getCarFollowers } from '../API/GetStream'
+import { getGetStreamToken } from '../API/user'
 import { getUserFollowingFeeds } from './user'
 import { getApiFetchLimit } from '../API/config'
 
@@ -147,3 +150,51 @@ export const unFollowCar = (userId, carInfoId) => {
             })
     }
 }
+
+export const getCarFollowersCount = (id) => {
+  return dispatch => {
+
+    dispatch({
+        type: SET_FOLLOWER_COUNT,
+        followersContent: {
+            carInfoId: id,
+            loadPending: false,
+        }
+    })
+
+    const body = { actorId: id, actorType: 'car' }
+    getGetStreamToken({ body }, {})
+      .then(tokenResponse => {
+
+        getCarFollowers(tokenResponse.token, id)
+          .then(carFollowersResponse => {
+            console.log(carFollowersResponse)
+            dispatch({
+              type: SET_FOLLOWER_COUNT,
+              followersContent: {
+                carInfoId: id,
+                count: carFollowersResponse.length
+              }
+            })
+          })
+          .catch(args => {
+              dispatch({
+                type: SET_FOLLOWER_COUNT,
+                followersContent: {
+                    carInfoId: id,
+                    loadPending: true,
+                }
+            })
+        })
+      })
+      .catch(args => {
+            dispatch({
+            type: SET_FOLLOWER_COUNT,
+            followersContent: {
+                carInfoId: id,
+                loadPending: true,
+            }
+        })
+    })
+  }
+};
