809244565f76885faf96c68b44b15c903a5e2481
Tibs <ttiberiup@gmail.com>
2017-05-07 07:18:00 -0700

    store authentication in app storage
    * store token in storage
    * hide auth page if token exists
    * minor refactors

--- src/Components/RootNav.tsx
+++ src/Components/RootNav.tsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react'
-import {
-  Navigator
-} from 'react-native'
-import {
-  MainNav
-} from './'
-import { SignIn } from './Scenes'
+import React, { Component } from 'react';
+import { Navigator } from 'react-native';
+import { MainNav } from './';
+import { SignIn } from './Scenes';
 import { connect } from '../Utils/connect';
 
 const configureScene = ({ sceneConfig, id }: any = {}) => {
   if (sceneConfig) {
     return sceneConfig
-  }
-
-  if (id === 'modal') {
+  } else if (id === 'modal') {
     return Navigator.SceneConfigs.FloatFromBottom
   }
 
@@ -25,14 +19,13 @@ export class RootNav extends Component<any, any> {
 
   renderScene(route, navigator) {
     const { dispatch, user } = this.props;
-    const routeId: string =  !user.token || !user.id ? null : 'main';
+    const routeId: string =  user.token || user.id ? 'main' : 'auth';
 
-    switch (routeId) {
-      case 'main':
-        return (<MainNav navigator={navigator} />);
-      default:
-        return (<SignIn navigator={navigator} user={user} dispatch={dispatch} />);
+    if (routeId === 'main') {
+      return <MainNav navigator={navigator} />;
     }
+
+    return <SignIn navigator={navigator} user={user} dispatch={dispatch} />;
   }
 
   render() {
