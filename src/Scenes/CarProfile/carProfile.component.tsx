038623e98b250eb1507be2450ed3a1d8e3161418
tibs <ttiberiup@gmail.com>
2017-05-17 21:06:18 +0300

    fix carProfile background color

--- src/Scenes/CarProfile/carProfile.component.tsx
+++ src/Scenes/CarProfile/carProfile.component.tsx
@@ -135,8 +135,8 @@ const getCarProfileComponent = (props) => {
   };
 
   const userInfoComponent = (
-    <View style={{ backgroundColor: background.carProfile }}>
-      <View style={styles.infoWrapper}>
+    <View style={styles.infoWrapper}>
+      <View style={styles.subWrapper}>
         <View style={styles.imageWrapper}>
           <FitImage source={{ uri: image }} round={true} />
         </View>
